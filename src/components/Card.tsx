a38e107eb34db44d18c4d8fc5877191392c09d16
kris <kmsedu@tuta.io>
2022-07-21 15:52:18 +1000

    Resized shadow on Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export default function Card (props: CardProps): JSX.Element {
   const { cat, handleCatClick } = props
 
   return (
-    <div className='shadow-sm shadow-neutral-500 rounded-xl'>
+    <div className='shadow-md shadow-neutral-400 rounded-xl'>
       <img
         src={cat.url}
         className='object-cover w-48 h-48 rounded-xl hover:brightness-90 hover:cursor-pointer'
