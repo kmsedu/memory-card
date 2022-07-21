19016ea566dd7d862682488283b7d2f4c176fd40
kris <kmsedu@tuta.io>
2022-07-21 15:50:16 +1000

    Added styling and fonts

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export default function Card (props: CardProps): JSX.Element {
   const { cat, handleCatClick } = props
 
   return (
-    <div className='shadow-xl rounded-xl'>
+    <div className='shadow-sm shadow-neutral-500 rounded-xl'>
       <img
         src={cat.url}
         className='object-cover w-48 h-48 rounded-xl hover:brightness-90 hover:cursor-pointer'
