299095d0621acd624cc5ee9c004788ebdb92c62f
kris <kmsedu@tuta.io>
2022-07-21 15:33:26 +1000

    Styling changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,6 @@ import { Cat } from '../App'
 
 interface CardProps {
   cat: Cat
-  toggleSelected: Function
   handleCatClick: Function
 }
 
@@ -10,10 +9,10 @@ export default function Card (props: CardProps): JSX.Element {
   const { cat, handleCatClick } = props
 
   return (
-    <div>
+    <div className='shadow-xl rounded-xl'>
       <img
         src={cat.url}
-        className='object-cover w-48 h-48 rounded-xl'
+        className='object-cover w-48 h-48 rounded-xl hover:brightness-90 hover:cursor-pointer'
         onClick={() => handleCatClick(cat.id)}
       />
     </div>
